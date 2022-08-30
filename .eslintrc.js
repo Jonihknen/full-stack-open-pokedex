3058871d8cab5df3eef19a6cb90eff9874412d8c
Joni <jonihknen@gmail.com>
2022-08-30 21:14:38 +0300

    some configuring

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,13 +4,11 @@ module.exports = {
     node: true,
     es6: true,
     "jest/globals": true,
+    "cypress/globals": true
+
   },
   extends: ["eslint:recommended", "plugin:react/recommended"],
-  eslintConfig: {
-    "extends": [
-      "plugin:cypress/recommended"
-    ]
-  },
+  
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
@@ -18,7 +16,7 @@ module.exports = {
     ecmaVersion: 2018,
     sourceType: "module",
   },
-  plugins: ["react", "jest"],
+  plugins: ["react", "jest", "cypress"],
   rules: {
     indent: ["error", 2],
     "linebreak-style": ['error', process.platform === 'win32' ? 'windows' : 'unix'],
